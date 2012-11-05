853f30181382be17b31e5b290117b4a54f9af6d4
Gianni Chiappetta <gianni@runlevel6.org>
2012-11-04 20:24:19 -0800

    Initial Commit

---
+++ page-ready/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  page-ready
+//
+//  Created by Gianni Chiappetta on 2012-11-04.
+//  Copyright (c) 2012 Gianni Chiappetta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
